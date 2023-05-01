4fe52c15f187d0ab44ff7aab4c792673ce974eb5
b06 <bee85919@gmail.com>
2023-05-01 14:26:04 +0900

    feat: add loop on selector

--- selector.py
+++ selector.py
@@ -5,7 +5,13 @@ def num_selector():
     return random.sample(range(1,46), 6)
 
 
-num_selected = num_selector()
+def print_num_selected():
+    num_selected = num_selector()
+    print("로또번호:", sorted(num_selected))
 
 
-print("로또 번호:", sorted(num_selected))
+while True:
+    print_num_selected()
+    press_y = input("새로운 로또번호를 생성하려면 'y' 또는 'Y'를 입력하세요. 종료하려면 다른 키를 누르세요: ")
+    if press_y.lower() != 'y':
+        break
