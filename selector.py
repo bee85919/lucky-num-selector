3ac957a783afc4214443e21c6532da0be0fe184b
b06 <bee85919@gmail.com>
2023-05-01 14:40:26 +0900

    feat: update var names

--- selector.py
+++ selector.py
@@ -1,17 +1,17 @@
 import random
 
 
-def num_selector():
+def get_num():
     return random.sample(range(1,46), 6)
 
 
-def print_num_selected():
-    num_selected = num_selector()
-    print("로또번호:", sorted(num_selected))
+def print_num():
+    nums = get_num()
+    print("로또번호:", sorted(nums))
 
 
 while True:
-    print_num_selected()
-    press_y = input("새로운 로또번호를 생성하려면 'y' 또는 'Y'를 입력하세요. 종료하려면 다른 키를 누르세요: ")
-    if press_y.lower() != 'y':
+    print_num()
+    retry_key = input("새로운 로또번호를 생성하려면 'y' 또는 'Y'를 입력하세요. 종료하려면 다른 키를 누르세요: ")
+    if retry_key.lower() != 'y':
         break
